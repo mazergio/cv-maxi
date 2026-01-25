2531062aaf9dff87a6135678e2da7880d90fecaa
Maxi Lopez <smaximilianolopez@gmail.com>
2026-01-25 12:00:52 -0300

    agregar hoja Configuracion como objeto global

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
+// =========================
+// CONFIGURACIÓN
+// =========================
 
+let config = {};
+
+const configURL =
+  "https://docs.google.com/spreadsheets/d/1Hx-C_mXVmLKO06n6MMt4bSjpT5jFLsmCqPw4SCR3kCY/export?format=csv&gid=1513454922";
+
+fetch(configURL)
+  .then(res => res.text())
+  .then(text => {
+
+    const filas = text
+      .split(/\r?\n/)
+      .map(f => f.trim())
+      .filter(Boolean)
+      .map(f =>
+        f.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/)
+         .map(c => c.replace(/^"|"$/g, "").trim())
+      );
+
+    for (let i = 1; i < filas.length; i++) {
+      const clave = filas[i][0];
+      const valor = filas[i][1];
+      if (clave) {
+        config[clave] = valor;
+      }
+    }
+
+    console.log("Config cargada:", config);
+  })
+  .catch(err => console.error("Error cargando config:", err));
+  
+
+  
   // =========================
   // PERFIL
   // =========================
